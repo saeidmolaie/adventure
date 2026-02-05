802da2ff521f2d35bf644385c6ac097fe29951ff
Saeid Molaie <saeedmolaiy.dev@gmail.com>
2026-02-05 23:07:40 +0330

    Placement New

---
+++ PlacementNew/PlacementNew.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include <malloc.h>
+
+size_t _size;
+
+struct Node
+{
+	int _a;
+
+	Node(const int& a)
+	{
+		_a = a;
+	}
+};
+
+int main()
+{
+	Node* buffer = (Node*)malloc(sizeof(Node) * 10);
+	Node* copyBuffer = (Node*)malloc(sizeof(Node) * 10);
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		new (buffer + i) Node(i);
+	}
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		new (copyBuffer + i) Node(buffer[i]);
+	}
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		std::cout << buffer[i]._a << std::endl;
+		std::cout << copyBuffer[i]._a << std::endl;
+	}
+
+	std::cin.get();
+}
